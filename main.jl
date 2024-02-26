bd49ceb72d55492bb284d12847c55452168aaab4
Piotr Zakrzewski <1050269+PiotrZakrzewski@users.noreply.github.com>
2024-02-26 13:00:25 +0100

    Lower the rent each month when vacant

--- main.jl
+++ main.jl
@@ -3,7 +3,7 @@ using Agents
 @agent Rental GridAgent{2} begin
     rent::Int
     quality::Int
-    minimum_price::Int
+    minimum_rent::Int
     months_vacant::Int
     months_occupied::Int
     tenant::Int
@@ -29,15 +29,20 @@ end
 function agent_step(agent::Rental, model)
     if agent.tenant == 0
         agent.months_vacant += 1
+        rent_decrease = getproperty(model, :rent_decrease_perc) * agent.rent
+        agent.rent -=  rent_decrease
+        agent.rent = max(agent.rent, agent.minimum_rent)
     else
         agent.months_occupied += 1
     end
+
+
     return
 end
 
 function init_model()
     space = GridSpaceSingle((10, 10); periodic=false)
-    model_properties = Dict(:contract_duration => 12, :max_rent_increase_perc => 0.05)
+    model_properties = Dict(:contract_duration => 12, :max_rent_increase_perc => 0.05, :rent_decrease_perc=> 0.05)
     market_model = ABM(Union{Rental,Renter}, space; properties=model_properties)
     return market_model
 end
--- sim_test.jl
+++ sim_test.jl
@@ -3,7 +3,7 @@ using Test
 include("main.jl")
 
 
-@testset "Test agent_step" begin
+@testset "Test agent_step: vacany duration tracking" begin
     test_model1 = init_model()
 
     rental1 = add_agent!((1, 2), Rental, test_model1, 1, 1, 1, 0, 0, 0)
@@ -20,4 +20,15 @@ include("main.jl")
     @test 1 == rental1.months_occupied
     @test 1 == renter1.months_renting
     @test 0 == rental1.months_vacant
+end
+
+@testset "Test agent_step: lowering rent when vacant" begin
+    test_model2 = init_model()
+
+    rental2 = add_agent!((1, 2), Rental, test_model2, 1000, 50, 500, 10, 0, 0)
+
+    step!(test_model2, agent_step, 1)
+    @test 11 == rental2.months_vacant
+    @test 0 == rental2.months_occupied
+    @test 950 == rental2.rent
 end
