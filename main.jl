c280162bafcf5928b63cb1fb95bb14c6d38e5572
Piotr Zakrzewski <1050269+PiotrZakrzewski@users.noreply.github.com>
2024-02-26 11:08:36 +0100

    Refactor to use multiple dispatch

--- main.jl
+++ main.jl
@@ -1,44 +1,43 @@
 using Agents
 
 @agent Rental GridAgent{2} begin
-rent::Int
-quality::Int
-minimum_price::Int
-months_vacant::Int
-months_occupied::Int
-tenant::Int
+    rent::Int
+    quality::Int
+    minimum_price::Int
+    months_vacant::Int
+    months_occupied::Int
+    tenant::Int
 end
 
 @agent Renter GridAgent{2} begin
-max_rent::Int
-min_quality::Int
-desired_quality::Int
-months_renting::Int
-address::NTuple{2, Int}
+    max_rent::Int
+    min_quality::Int
+    desired_quality::Int
+    months_renting::Int
+    address::NTuple{2,Int}
 end
 
 OUT_OF_TOWN = (1, 1)
 
-function agent_step(agent, model)
-    neighbours = Agents.nearby_agents(agent, model, 1)
-    if agent isa Rental
-        if agent.tenant == 0
-            agent.months_vacant += 1
-        else
-            agent.months_occupied += 1
-        end
-    elseif agent isa Renter
-        if agent.address != OUT_OF_TOWN
-            agent.months_renting += 1
-        end
+function agent_step(agent::Renter, model)
+    if agent.address != OUT_OF_TOWN
+        agent.months_renting += 1
+    end
+    return
+end
+
+function agent_step(agent::Rental, model)
+    if agent.tenant == 0
+        agent.months_vacant += 1
+    else
+        agent.months_occupied += 1
     end
     return
 end
 
 function init_model()
-    space = GridSpaceSingle((10, 10); periodic = false)
+    space = GridSpaceSingle((10, 10); periodic=false)
     model_properties = Dict(:contract_duration => 12, :max_rent_increase_perc => 0.05)
-    market_model = ABM(Union{Rental, Renter}, space; properties = model_properties)
+    market_model = ABM(Union{Rental,Renter}, space; properties=model_properties)
     return market_model
 end
-    
--- sim_test.jl
+++ sim_test.jl
@@ -2,19 +2,22 @@ using Test
 
 include("main.jl")
 
-test_model1 = init_model()
 
-rental1 = add_agent!((1, 2), Rental, test_model1, 1, 1, 1, 0, 0, 0)
-renter1 = add_agent!((1, 1), Renter, test_model1, 1, 1, 1, 0, OUT_OF_TOWN)
+@testset "Test agent_step" begin
+    test_model1 = init_model()
 
-rental1.tenant = renter1.id
-renter1.address = (1, 2)
-@test 0 == rental1.months_vacant
-@test 0 == rental1.months_occupied
-@test 0 == renter1.months_renting
+    rental1 = add_agent!((1, 2), Rental, test_model1, 1, 1, 1, 0, 0, 0)
+    renter1 = add_agent!(OUT_OF_TOWN, Renter, test_model1, 1, 1, 1, 0, OUT_OF_TOWN)
 
-step!(test_model1, agent_step, 1)
+    rental1.tenant = renter1.id
+    renter1.address = (1, 2)
+    @test 0 == rental1.months_vacant
+    @test 0 == rental1.months_occupied
+    @test 0 == renter1.months_renting
 
-@test 1 == rental1.months_occupied
-@test 1 == renter1.months_renting
-@test 0 == rental1.months_vacant
+    step!(test_model1, agent_step, 1)
+
+    @test 1 == rental1.months_occupied
+    @test 1 == renter1.months_renting
+    @test 0 == rental1.months_vacant
+end
